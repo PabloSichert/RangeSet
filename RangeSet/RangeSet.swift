1df97d3de8300c0ff83a0dcaa6435bb52bb53caf
Pablo Sichert <mail@pablosichert.com>
2018-10-07 00:56:38 +0200

    Implement base range set

--- RangeSet/RangeSet.swift
+++ RangeSet/RangeSet.swift
@@ -1,1 +1,66 @@
-public struct RangeSet<Bound> {}
+/**
+ A range that has a well defined representation for empty, conjunct and disjunct ranges.
+ */
+public struct RangeSet<Bound: Comparable> {
+    /**
+     The disjunct ranges that compose the range set.
+
+     These ranges are guaranteed to be
+     - disjunct
+     - ordered by lower bound, ascending
+
+     Where the number of ranges in the array has a defined meaning:
+
+     - `range.count == 0`: empty range set
+     - `range.count == 1`: set with one conjunct range
+     - `range.count > 1`: set with multiple disjoint ranges
+     */
+    public let ranges: [Range<Bound>]
+
+    /**
+     The lowest lower bound in the range set.
+
+     `nil` when the range is empty.
+     */
+    public var lowerBound: Bound? {
+        return ranges.first?.lowerBound
+    }
+
+    /**
+     The upmost upper bound in the range set
+
+     `nil` when the range is empty.
+     */
+    public var upperBound: Bound? {
+        return ranges.last?.upperBound
+    }
+
+    /**
+     Returns `true` when the empty set is represented.
+     */
+    public var isEmpty: Bool {
+        return ranges.isEmpty
+    }
+
+    /**
+     Creates an empty range set.
+     */
+    public init() {
+        ranges = []
+    }
+
+    /**
+     Creates a range set from an existing range.
+     */
+    public init(_ range: Range<Bound>) {
+        guard range.lowerBound < range.upperBound else {
+            ranges = []
+
+            return
+        }
+
+        ranges = [
+            range
+        ]
+    }
+}
--- RangeSet/RangeSet.test.swift
+++ RangeSet/RangeSet.test.swift
@@ -1,4 +1,49 @@
 import XCTest
 import RangeSet
 
-class RangeSetTests: XCTestCase {}
+class RangeSetTests: XCTestCase {
+    func testInit() {
+        let range = RangeSet<Int>()
+
+        XCTAssert(range.ranges.count == 0)
+    }
+
+    func testInitWithEmptyRange() {
+        let range = RangeSet(0..<0)
+
+        XCTAssert(range.ranges.count == 0)
+    }
+
+    func testInitWithDenseRange() {
+        let range = RangeSet(0..<1)
+
+        XCTAssert(range.ranges.count == 1)
+        XCTAssert(range.ranges[0] == 0..<1)
+    }
+
+    func testEmptyEmptyRange() {
+        let range = RangeSet<Int>()
+
+        XCTAssert(range.isEmpty)
+    }
+
+    func testEmptyDenseRange() {
+        let range = RangeSet(0..<1)
+
+        XCTAssert(!range.isEmpty)
+    }
+
+    func testBoundsEmptyRange() {
+        let range = RangeSet(0..<0)
+
+        XCTAssert(range.lowerBound == nil)
+        XCTAssert(range.upperBound == nil)
+    }
+
+    func testBoundsDenseRange() {
+        let range = RangeSet(0..<1)
+
+        XCTAssert(range.lowerBound == 0)
+        XCTAssert(range.upperBound == 1)
+    }
+}
